734474222fe58211a0a23c0fa8d6281dd99d4e2a
2JK <rotorl106@gmail.com>
2020-02-20 22:56:30 +0900

    Initial commit

---
+++ app/src/main/java/com/example/memo/Memo.java
@@ -0,0 +1,44 @@
+package com.example.memo;
+
+import android.graphics.drawable.Drawable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Memo {
+
+    private String title;
+    private String contents;
+    private List<Drawable> image;
+
+    public Memo(String title, String contents){
+        this.title = title;
+        this.contents = contents;
+        image = new ArrayList<Drawable>();
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public List<Drawable> getImage() {
+        return image;
+    }
+
+    public void setImage(List<Drawable> image) {
+        this.image = image;
+    }
+}
